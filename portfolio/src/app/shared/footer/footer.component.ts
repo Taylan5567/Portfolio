89a3efbe3a17541db79c74ff2e873db72264095e
Özgür Taylan Umucu <59730168+Taylan5567@users.noreply.github.com>
2025-05-05 19:10:46 +0200

    new Project and Header and Footer

---
+++ portfolio/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {}
