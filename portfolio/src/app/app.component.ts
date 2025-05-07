bab118dd367c84ffb3c1d21b9736e4f8ac5b714b
Özgür Taylan Umucu <59730168+Taylan5567@users.noreply.github.com>
2025-05-07 15:38:16 +0200

    changes

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -3,14 +3,42 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    ContactComponent,
+    AboutComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+
+  ngOnInit(): void {
+    this.onHtmlLanguage('en');
+    if (navigator.language === 'de') {
+      this.onHtmlLanguage('de');
+    }
+  }
+
+  //wichtig für später
+  onHtmlLanguage(lang: string) {
+    const htmlElement = document.documentElement;
+    htmlElement.setAttribute('lang', lang);
+  }
 }
